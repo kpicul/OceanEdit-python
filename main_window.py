8010d5f186a365af43b585a67d89ea4231cc806d
kpiculin <kristjan.piculin@gmail.com>
2022-04-29 18:34:53 +0200

    Adding tabs.

--- main_window.py
+++ main_window.py
@@ -1,17 +1,18 @@
 import traceback
-from PyQt6.QtWidgets import QMainWindow, QPlainTextEdit
+from PyQt6.QtWidgets import QMainWindow, QTabWidget
 from PyQt6.QtGui import QAction
 from PyQt6 import uic
 from file_operations import read_file, write_file
 from dialogs import open_file_dialog, show_error_dialog, show_about_dialog
 from open_dialog_mode import OpenDialogMode
+from text_area import TextArea
 
 
 class MainWindow(QMainWindow):
     """Main window class.
 
     Attributes:
-        main_area (QPlainTextEdit): Main text area.
+        editor_tabs (QTabWidget): Tab widget that contains open editors.
         action_open (QAction): Action that activates open function.
         action_save (QAction): Action that saves file.
         action_copy (QAction): Action that activates copy function.
@@ -22,8 +23,11 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("forms/mainWindow.ui", self)
-        self.main_area: QPlainTextEdit = self.findChild(QPlainTextEdit, "mainArea")
 
+        # Elements
+        self.editor_tabs: QTabWidget = self.findChild(QTabWidget, "editorTabs")
+
+        # Actions
         self.action_open: QAction = self.findChild(QAction, "actionOpen")
         self.action_save: QAction = self.findChild(QAction, "actionSave")
         self.action_copy: QAction = self.findChild(QAction, "actionCopy")
@@ -38,9 +42,9 @@ class MainWindow(QMainWindow):
         """Sets the main window events."""
         self.action_open.triggered.connect(self.open_file)
         self.action_save.triggered.connect(self.save_file)
-        self.action_copy.triggered.connect(self.main_area.copy)
-        self.action_paste.triggered.connect(self.main_area.paste)
-        self.action_cut.triggered.connect(self.main_area.cut)
+        self.action_copy.triggered.connect(self.copy_operation)
+        self.action_paste.triggered.connect(self.paste_operation)
+        self.action_cut.triggered.connect(self.cut_operation)
         self.action_about.triggered.connect(show_about_dialog)
 
     def open_file(self):
@@ -48,9 +52,9 @@ class MainWindow(QMainWindow):
         try:
             file_path = open_file_dialog(self, "Select file", OpenDialogMode.OPEN, "")
             if len(file_path) != 0:
-                content: str = read_file(file_path)
-                self.main_area.setPlainText(content)
-        except:
+                new_text_area = self.add_new_tab(file_path)
+                new_text_area.set_content(file_path)
+        except FileNotFoundError:
             show_error_dialog(traceback.format_exc())
 
     def save_file(self):
@@ -58,7 +62,39 @@ class MainWindow(QMainWindow):
         try:
             file_path = open_file_dialog(self, "Select file", OpenDialogMode.WRITE)
             if len(file_path) != 0:
-                content: str = self.main_area.toPlainText()
-                write_file(file_path, content)
-        except:
+                self.get_current_tab().save_content(True, file_path)
+        except FileNotFoundError:
             show_error_dialog(traceback.format_exc())
+
+    def add_new_tab(self, file_name: str = "new file") -> TextArea:
+        """Adds new tab and sets it as selected.
+
+        Args:
+            file_name (str): File name. New file as default.
+
+        Returns:
+            TextArea: new editor.
+        """
+        new_text_area = TextArea()
+        self.editor_tabs.addTab(new_text_area, file_name)
+        self.editor_tabs.setCurrentWidget(new_text_area)
+        return new_text_area
+
+    def get_current_tab(self) -> TextArea:
+        """Returns current editor.
+
+        Returns:
+            TextArea: current editor.
+        """
+        return self.editor_tabs.currentWidget()
+
+    def copy_operation(self):
+        if self.editor_tabs.count() > 0:
+            self.get_current_tab().copy_operation()
+
+    def paste_operation(self):
+        if self.editor_tabs.count() > 0:
+            self.get_current_tab().paste_operation()
+
+    def cut_operation(self):
+        self.get_current_tab().cut_operation()
---
+++ text_area.py
@@ -0,0 +1,54 @@
+from PyQt6.QtWidgets import QWidget, QPlainTextEdit
+from PyQt6 import uic
+from file_operations import read_file, write_file
+
+
+class TextArea(QWidget):
+    """Text area which contains the text.
+
+    Attributes
+        editor_area (QPlainTextEdit): Area that contains the text.
+        file_path (str): Path of the file or empty string if file is not yet saved.
+    """
+
+    def __init__(self):
+        super(TextArea, self).__init__()
+        uic.loadUi("forms/textArea.ui", self)
+
+        # Setting the properties.
+        self.editor_area: QPlainTextEdit = self.findChild(QPlainTextEdit, "editorArea")
+        self.file_path: str = ""
+
+    def set_content(self, file_path: str):
+        """Sets the area content.
+
+        Args:
+            file_path (str): path of the file which contains the content..
+        """
+        self.file_path = file_path
+        content: str = read_file(file_path)
+        self.editor_area.setPlainText(content)
+
+    def save_content(self, new_file: bool = False, file_path: str = ""):
+        """Saves the content of the file and if it is a new file sets the file path.
+
+        Args:
+            new_file (bool): if it is a new file. False by default.
+            file_path (str): path of the file.
+        """
+        if new_file:
+            self.file_path = file_path
+        content: str = self.editor_area.toPlainText()
+        write_file(file_path, content)
+
+    def copy_operation(self):
+        """Performs copy operation."""
+        self.editor_area.copy()
+
+    def paste_operation(self):
+        """Performs paste operation."""
+        self.editor_area.paste()
+
+    def cut_operation(self):
+        """Performs cut operation."""
+        self.editor_area.cut()
